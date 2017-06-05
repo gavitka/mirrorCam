1c02818944d1f2ef6bd81961f03791ec10a6dd55
gavitka <aaliapkin@gmail.com>
2017-06-06 01:04:14 +0300

    camera working

--- app/src/main/java/com/example/myfirstapp/CameraHelper.java
+++ app/src/main/java/com/example/myfirstapp/CameraHelper.java
@@ -1,13 +1,22 @@
 package com.example.myfirstapp;
 
+import android.Manifest;
 import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
 import android.support.annotation.NonNull;
 import android.util.Log;
 import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.util.Arrays;
 
 /**
  * Created by Kheops on 6/5/2017.
@@ -16,6 +25,57 @@ import android.util.Size;
 public class CameraHelper {
     private CameraManager mCameraManager    = null;
     private String mCameraID    = null;
+
+    private CameraDevice mCameraDevice  = null;
+    private CameraCaptureSession mSession;
+    private TextureView mTextureView;
+
+    public boolean isOpen() {
+        if (mCameraDevice == null) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void openCamera() {
+        try {
+            mCameraManager.openCamera(mCameraID,mCameraCallback,null);
+        } catch (CameraAccessException e) {
+            Log.e(MainActivity.LOG_TAG,e.getMessage());
+            //e.printStackTrace();
+        }
+    }
+
+    public void closeCamera() {
+
+        if (mCameraDevice != null) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+        }
+    }
+
+    private CameraDevice.StateCallback mCameraCallback = new CameraDevice.StateCallback() {
+
+        @Override
+        public void onOpened(CameraDevice camera) {
+            mCameraDevice = camera;
+            createCameraPreviewSession();
+            Log.i(MainActivity.LOG_TAG, "Open camera  with id:"+mCameraDevice.getId());
+        }
+
+        @Override
+        public void onDisconnected(CameraDevice camera) {
+            mCameraDevice.close();
+            mCameraDevice = null;
+            Log.i(MainActivity.LOG_TAG, "disconnect camera  with id:"+mCameraDevice.getId());
+        }
+
+        @Override
+        public void onError(CameraDevice camera, int error) {
+            Log.i(MainActivity.LOG_TAG, "error! camera id:"+camera.getId()+" error:"+error);
+        }
+    };
 
     public CameraHelper(@NonNull CameraManager cameraManager, @NonNull String cameraID) {
         mCameraManager  = cameraManager;
@@ -47,6 +107,50 @@ public class CameraHelper {
             Log.e(MainActivity.LOG_TAG,e.getMessage());
             //e.printStackTrace();
         }
+    }
+
+    private void createCameraPreviewSession() {
+
+        SurfaceTexture texture = mTextureView.getSurfaceTexture();
+        texture.setDefaultBufferSize(1920,1080);
+        Surface surface = new Surface(texture);
+
+        try {
+            final CaptureRequest.Builder builder =
+                    mCameraDevice.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+
+            builder.addTarget(surface);
+
+            mCameraDevice.createCaptureSession(
+                    Arrays.asList(surface),
+                    new CameraCaptureSession.StateCallback() {
+
+                        @Override
+                        public void onConfigured(CameraCaptureSession session) {
+                            mSession = session;
+                            try {
+                                mSession.setRepeatingRequest(builder.build(),null,null);
+                            } catch (CameraAccessException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onConfigureFailed(CameraCaptureSession session) {
+                        }
+
+                    },
+                    null
+
+            );
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public void setTextureView(TextureView value) {
+        mTextureView = value;
     }
 
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.myfirstapp;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.graphics.ImageFormat;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.util.Size;
+import android.view.Surface;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
 
     private CameraManager mCameraManager    = null;
     static final String LOG_TAG = "ssibal";
-    CameraHelper myCameras[];
+    private static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 1;
+    private final int CAMERA1   = 0;
+    private final int CAMERA2   = 1;
+    private CameraHelper[] myCameras = null;
+    private Button mButtonOpenCamera1 = null;
+    private Button mButtonOpenCamera2 = null;
+    private TextureView mImageView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mCameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        mImageView = (TextureView) findViewById(R.id.image_view);
 
         try {
             // Получения списка камер в устрйстве
@@ -37,13 +58,52 @@ public class MainActivity extends AppCompatActivity {
 
                 // выводим инормацию по камере
                 myCameras[id].viewFormatSize(ImageFormat.JPEG);
+                myCameras[id].setTextureView(mImageView);
             }
         } catch (CameraAccessException e) {
             Log.e(LOG_TAG,e.getMessage());
             e.printStackTrace();
         }
 
+        //requestPermissions(new String[]{Manifest.permission.CAMERA}, REQ_ACCESS_FINE_LOCATION);
 
+        mButtonOpenCamera1 = (Button) findViewById(R.id.btn_open_camera1);
+        mButtonOpenCamera2 = (Button) findViewById(R.id.btn_open_camera2);
+        mButtonOpenCamera1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myCameras[CAMERA2].isOpen()) myCameras[CAMERA2].closeCamera();
+                if (myCameras[CAMERA1] != null) {
+                    if (!myCameras[CAMERA1].isOpen()) {
+                        myCameras[CAMERA1].openCamera();
+                    }
+                }
+            }
+        });
 
+        mButtonOpenCamera2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (myCameras[CAMERA1].isOpen()) myCameras[CAMERA1].closeCamera();
+                if (myCameras[CAMERA2] != null) {
+                    if (!myCameras[CAMERA2].isOpen()) myCameras[CAMERA2].openCamera();
+                }
+            }
+        });
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.CAMERA)
+                != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.CAMERA)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.CAMERA},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        }
     }
+
+
+
 }
